f99f60332678d9d8ab5c8cab7e4617f91fd65ee8
TorinoSM <foracad@mail.ru>
2018-01-19 00:29:49 +0300

    HW4: added exception classes, modified methods due to exceptions, added mainreflection class, added AbstractStorageTest, ArrayStorageTest, SortedArrayStorageTest classes, modified delete() method, invoked Resume.toString() method via reflection

---
+++ src/for_play.java
@@ -0,0 +1,30 @@
+import java.util.SortedMap;
+import java.util.Iterator;
+import java.nio.charset.Charset;
+
+
+/**
+ * CharsetTest
+ *
+ * @author Eugene Matyushkin
+ */
+public class for_play {
+
+    public static void main(String[] args){
+        SortedMap<String, Charset> charsetsMap = Charset.availableCharsets();
+        System.out.println("Charsets available: "+charsetsMap.size());
+        for(String name : charsetsMap.keySet()){
+            Charset charset = charsetsMap.get(name);
+            StringBuffer sb = new StringBuffer();
+            sb.append(name);
+            sb.append(" (");
+            for(Iterator<String> aliases = charset.aliases().iterator();aliases.hasNext();){
+                sb.append(aliases.next());
+                if (aliases.hasNext())
+                    sb.append(",");
+            }
+            sb.append(")");
+            System.out.println(sb.toString());
+        }
+    }
+}

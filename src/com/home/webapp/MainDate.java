ada10f69b9cc113d429794f1a2b19a46f7c267b3
TorinoSM <foracad@mail.ru>
2018-04-17 20:35:09 +0300

    add Maven support

--- src/com/home/webapp/MainDate.java
+++
@@ -1,8 +0,0 @@
-package com.home.webapp;
-
-import java.util.Date;
-
-public class MainDate {
-    Date date = new Date();
-
-}
--- src/com/home/webapp/exception/NotExistStorageException.java
+++
@@ -1,7 +0,0 @@
-package com.home.webapp.exception;
-
-public class NotExistStorageException extends StorageException {
-    public NotExistStorageException(String uuid) {
-        super("Warning: Resume with uuid = \"" + uuid + "\" not found", uuid);
-    }
-}
